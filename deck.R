d927f937126c2f6d918f53da04eeb74145efe88a
chenghao <chenghao@bj-fanuc.com.cn>
2018-01-07 18:23:33 +0800

    shuttle card in new environment

--- deck.R
+++ deck.R
@@ -1,8 +1,21 @@
-shuffle<-function(cards){
-    random<-sample(1:52, size=52)
-    cards[random,]
-}
-
-deal<-function(cards){
-    cards[1,]
+setup<-function(deck){
+    Deck=deck
+    
+    shuffle<-function(){
+        random<-sample(1:52, size=52)
+        assign("Deck", Deck[random,], envir=parent.env( environment()))
+    }
+    
+    deal<-function(){
+        card<-Deck[1,]
+        assign("Deck", Deck[-1, ], envir = parent.env( environment()))
+        card
+    }
+    
+    
+    show_env<-function(){
+       list(ran.in = environment(), parent=parent.env( environment()),
+            objects = ls.str(environment()),globalevnstr=globalenv())
+    }
+    list(shuffles=shuffle, deal=deal, show_env=show_env)
 }
